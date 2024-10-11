7feedf2a2547d8166433f2139d9a449da696c950
IvanSlivenko <ivan8822@ukr.net>
2024-10-11 17:03:57 +0300

    11-10-2024-17-03

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+//Constans
 const PORT = process.env.PORT;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD =process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
+const DB_NAME_2= process.env.DB_NAME_2
 
 const app = express();
 dotenv.config();
@@ -14,6 +16,7 @@ async function start() {
     try{
         await mongoose.connect(
             `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.jgevz.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+            // `mongodb+srv://umanproger:${DB_PASSWORD}@cluster0.jgevz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
         )
         app.listen(PORT, ()=>{
             console.log(`Server run in http://localhost:${PORT}`);
