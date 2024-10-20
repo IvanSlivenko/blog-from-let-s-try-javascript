449633f60c38b03c0894a34923e21c8536f873b6
IvanSlivenko <ivan8822@ukr.net>
2024-10-21 01:20:03 +0300

    21-10-2024-01-19

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import { Routes, Route} from 'react-router-dom'
 
+import { Layout } from './components/Layout'
+import { MainPage } from './pages/MainPage'
+import { PostsPage } from './pages/PostsPage'
+import { PostPage } from './pages/PostPage'
+import { AddPostPage } from './pages/AddPostPage'
+import { EditPostPage } from './pages/EditPostPage'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
 function App() {
   return (
-    <div className="text-2xl text-red-600">
-      <h1>test</h1>
-    </div>
-  );
+    <Layout>
+      <Routes>
+        <Route path='/' element={ <MainPage/>}/>
+        <Route path='posts' element={ <PostsPage/>}/>
+        <Route path=':id' element={<PostPage/>}/>
+        <Route path='new' element={<AddPostPage/>}/>
+        <Route path=':id/edit' element={<EditPostPage/>}/>
+        <Route path='register' element={<RegisterPage/>}/>
+        <Route path='login' element={<LoginPage/>}/>
+      </Routes>
+    </Layout>
+  )
 }
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <div className='flex py-4 justify-between items-center'>
+      <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>E</span>
+
+      <ul className="flex gap-8">
+        <li>
+          <NavLink 
+            to={'/'}
+            className='text-xs text-gray-400 hover:text-white'
+            >
+              Головна
+            </NavLink>
+          </li>
+
+          <li>
+          <NavLink 
+            to={'/posts'}
+            className='text-xs text-gray-400 hover:text-white'
+            >
+              Мої статті
+            </NavLink>
+          </li>
+
+          <li>
+          <NavLink 
+            to={'/new'}
+            className='text-xs text-gray-400 hover:text-white'
+            >
+              Додати статтю
+            </NavLink>
+          </li>
+      </ul>
+
+      <div className=' flex  justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2'>
+        <button>Увійти</button>
+      </div>
+    </div>
+  )
+}
